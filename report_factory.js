30bd0f7e25c50e8f8a1a440d49cb29ce105478cf
igor-starostenko <contactigorstar@gmail.com>
2019-01-20 23:48:41 -0800

    Mocha reporter

---
+++ report_factory.js
@@ -0,0 +1,79 @@
+// report-factory.js
+
+const mocha = require('mocha');
+const { sendReport } = require('./lib/api');
+
+const successMessage = 'Report has been successfully submitted to ReportFactory\n'
+
+/**
+ * Return a plain-object representation of `test`
+ * free of cyclic properties etc.
+ *
+ * @private
+ * @param {Object} test
+ * @return {Object}
+ */
+function formatDetails(test) {
+  let err = test.err || null;
+
+  return {
+    title: test.title,
+    fullTitle: test.fullTitle(),
+    duration: test.duration,
+    currentRetry: test.currentRetry(),
+    err: err,
+  };
+}
+
+/**
+ * Initialize a new `ReportFactory` reporter.
+ *
+ * @public
+ * @class ReportFactory
+ * @memberof Mocha.reporters
+ * @extends Mocha.reporters.Base
+ * @param {Runner} runner
+ */
+function ReportFactory(runner) {
+  mocha.reporters.Base.call(this, runner);
+
+  let self = this;
+  let tests = [];
+  let pending = [];
+  let failures = [];
+  let passes = [];
+
+  runner.on('test end', function(test) {
+    tests.push(test);
+  });
+
+  runner.on('pass', function(test) {
+    passes.push(test);
+  });
+
+  runner.on('fail', function(test) {
+    failures.push(test);
+  });
+
+  runner.on('pending', function(test) {
+    pending.push(test);
+  });
+
+  runner.once('end', function() {
+    const report = {
+      stats: self.stats,
+      tests: tests.map(formatDetails),
+      pending: pending.map(formatDetails),
+      failures: failures.map(formatDetails),
+      passes: passes.map(formatDetails)
+    };
+    runner.testResults = report;
+
+    sendReport(report)
+      .then(() => process.stdout.write(successMessage))
+      .catch(error => console.error(error));
+  });
+}
+
+ReportFactory.description = 'submits test results to ReportFactory';
+module.exports = ReportFactory;
