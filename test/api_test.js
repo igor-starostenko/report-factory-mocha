a6e50cf6b765e476027ddaf0479fab20017ce6a9
igor-starostenko <contactigorstar@gmail.com>
2019-01-21 21:33:45 -0800

    Renamed start, end to started and ended

--- test/api_test.js
+++ test/api_test.js
@@ -5,7 +5,7 @@ const { configure } = require('../lib/config');
 
 describe('ReportFactory', function() {
   const baseUrl = 'http://0.0.0.0:3000';
-  const projectName = 'webapp';
+  const projectName = 'x-webapp';
   const path = `/api/v1/projects/${projectName}/reports/mocha`;
   const authToken = '82f551b8-dea9-4385-9c4f-d290688391cc';
 
@@ -20,8 +20,8 @@ describe('ReportFactory', function() {
       passes: 1,
       pending: 0,
       failures: 0,
-      start: '2019-01-20T04:28:34.861Z',
-      end: '2019-01-20T04:28:34.867Z',
+      started: '2019-01-20T04:28:34.861Z',
+      ended: '2019-01-20T04:28:34.867Z',
       duration: 6,
       tests: [{
         title: 'submits a passed test report to ReportFactory',
@@ -30,7 +30,7 @@ describe('ReportFactory', function() {
         duration: 18,
         status: 'passed',
         speed: 'fast',
-        file: '/Users/igorstar/Workspace/ReportFactory/report-factory-mocha/test/api_test.js',
+        file: '/Users/username/ReportFactory/report-factory-mocha/test/api_test.js',
         timedOut: false,
         pending: false,
         sync: true,
@@ -57,12 +57,13 @@ describe('ReportFactory', function() {
     it('submits a failed test report to ReportFactory', () => {
       const failedReport = { ...passedReport };
       const error = 'ReferenceError: response is not defined\nat Context.beforeEach (test/api_test.js:10:21) } ]';
+      failedReport.tests[0].status = 'failed';
       failedReport.tests[0].err = error;
 
       return sendReport(failedReport)
         .then(response => {
           expect(typeof response).to.equal('object');
-          expect(response.status).to.equal('OK')
+          expect(response.status).to.equal('NO')
         });
     });
   });
